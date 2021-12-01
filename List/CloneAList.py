e89acdeb4eb4d91a621926471162b1e40bdd6a3d
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:38:32 +0530

    [Subhadeep] add clone list

---
+++ List/CloneAList.py
@@ -0,0 +1,13 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-26 11:00
+	@Title : To make a clone of the list
+	
+'''
+
+list = ["Apple", "Mango", "Banana", "Sugarcane", "Apple"]
+new_list = list
+
+print(list)
+print("After cloning/copying")
+print(new_list)
