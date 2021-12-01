f79f8b99af6f996d384b8d13c87a515568ffe0e3
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:41:32 +0530

    [Subhadeep] add clear set prog

---
+++ Sets/ClearASet.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 17:10
+	@Title : To clear a set
+	
+'''
+new_set = {10,20,30,40,50,60}
+print(new_set)
+print("After clearing the set ")
+new_set.clear()
+print(new_set)
