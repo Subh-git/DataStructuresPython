a9eb56d742abb4a17b133dc0d90994fd2eccc7f8
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:45:54 +0530

    [Subhadeep] add converting to a list

---
+++ Tuple/ConvertListToTuple.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 12:35
+	@Title : To check convert a list to a tuple
+	
+'''
+
+lst = ["Apple", "XYZ", "Sun", "Moon"]
+print(lst)
+tuple = tuple(lst)
+print(tuple)
