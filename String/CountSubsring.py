c0974dfa70763c7ad7baf57236f773ba5c6aa13f
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:44:05 +0530

    [Subhadeep] add count substring

---
+++ String/CountSubsring.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 12:10
+	@Title : To count a substring from a string.
+	
+'''
+
+string = '''The human brain is the most crucial element of the body. This means that the brain controls the body
+            and without the brain, our body is of no use.
+            '''
+print(string.count('the'))
