9fd546300e3a8986f86d3b5013789a093c14b33c
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:42:42 +0530

    [Subhadeep] add remove from set prog

---
+++ Sets/RemoveFromSet.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 15:45
+	@Title : To remove elements from a set
+	
+'''
+new_set = {"Apple", "Cherry", "Tomatoes", "Avocado", "Watermelon"}
+
+#remove an element from set using discard
+new_set.discard("Apple")
+print(new_set)   
