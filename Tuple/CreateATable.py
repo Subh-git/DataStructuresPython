c3f35ef9b9d3deed803fbe9fea6bb97cf0f7cafd
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:46:04 +0530

    [Subhadeep] add creating a tuple

---
+++ Tuple/CreateATable.py
@@ -0,0 +1,15 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 10:00
+	@Title : To create a tuple
+	
+'''
+#declaration of tuple
+tup = ()
+#tuples are immutable so after declration we cant add values in it and neither can we delete it
+#tuples are faster than lists and provides readonly access to data
+
+tuple1 = ("Geeks","Jungle", "Abc")
+print(tuple1)
+tuple2 =(20,)   #for inserting single element
+print(type(tuple2))
