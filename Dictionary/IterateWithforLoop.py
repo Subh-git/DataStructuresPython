f2c0a95461e521cf342f2d81d258579acb214095
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:34:04 +0530

    [Subhadeep] add iterate with for loop prog

---
+++ Dictionary/IterateWithforLoop.py
@@ -0,0 +1,10 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 20:30
+	@Title : To iterate over a dictionary with for loop
+	
+'''
+new_dict = {"Apple": 20, "Orange" : 15, "Sugar": 25, "Green":34}
+
+for k,v in new_dict.items():
+    print(f"{k} is having value {v}")
