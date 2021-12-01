e674a96f5fb088f584541e59e29ff90a1f22b8f0
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:44:58 +0530

    [Subhadeep] add reversing astring prog

---
+++ String/ReverseString.py
@@ -0,0 +1,9 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 12:30
+	@Title : To reverse a given string.
+	
+'''
+
+str = input("Please enter your string: ")
+print("Reversed string is: ", str[::-1])
