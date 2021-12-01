fd4e83795fd56a3940e639af2d15634d5a04cf7c
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:43:41 +0530

    [Subhadeep] add calculate length

---
+++ String/CalculateLength.py
@@ -0,0 +1,9 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 09:34
+	@Title : To print the length of a string
+	
+'''
+
+string = input("Please enter the string whoose length you want--- ")
+print("The lenght of the string is: ",len(string))
