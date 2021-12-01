51e01fcc5aa22c4b540033efeff1dc8a7ff35998
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:32:26 +0530

    [Subhadeep] add new key prog

---
+++ Dictionary/AddKey.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 19:45
+	@Title : To add a new key to the dictionary
+	
+'''
+
+new_dict = {0: 10, 1: 20}
+print(new_dict)
+new_dict[2]=30
+print("After adding key-- ", new_dict)
