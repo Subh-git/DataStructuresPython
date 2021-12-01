d50becd68edb4fa29fb5261d591989cf0b2ef124
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:42:25 +0530

    [Subhadeep] add iteration of sets prog

---
+++ Sets/IterationOverSets.py
@@ -0,0 +1,10 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 14:25
+	@Title : To iterate over a given set
+	
+'''
+new_set = {10,20,30,40,50,60}
+
+for value in new_set:
+    print(value)
