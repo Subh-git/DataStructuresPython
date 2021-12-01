c1f2cd5c1c6b3c2a45681043d345ab1c87ddcccd
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:41:43 +0530

    [Subhadeep] add create a set prog

---
+++ Sets/CreateSet.py
@@ -0,0 +1,16 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 14:20
+	@Title : To create a set
+	
+'''
+
+#1st way of cretaing
+new_set = {"Box", "Van", "Car", "Bike"}
+print(new_set)
+
+#2nd way - by adding data later
+new_set.add("Hiking")
+print(new_set)
+
+#sets are unordered, unindexed, unchangeable, but we can add and remove. Also no duplicates allowed.
