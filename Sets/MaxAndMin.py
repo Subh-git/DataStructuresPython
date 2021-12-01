701f311afe7e931f18a803b09199d363c7b31fab
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:42:33 +0530

    [Subhadeep] add max and min prog

---
+++ Sets/MaxAndMin.py
@@ -0,0 +1,11 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-30 18:35
+	@Title : To obtain max and min value
+	
+'''
+
+new_set = {10,20,30,45,100}
+print(new_set)
+print("The max value is ", max(new_set))
+print("The minimum value is ", min(new_set))
