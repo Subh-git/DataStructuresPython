ee03e63bb88ed26585991d333a9d2dd89855c557
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 13:45:26 +0530

    [Subhadeep] add upper and lower case

---
+++ String/UpperAndLowerCases.py
@@ -0,0 +1,10 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 10:45
+	@Title : To show the user entered word in upper and lower cases.
+	
+'''
+
+string = input("Please enter the string: ")
+print("Upper case---",string.upper())
+print("Lower case--- ",string.lower())
